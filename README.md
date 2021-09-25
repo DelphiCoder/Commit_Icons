ad8a2fb82b946a4a9cf715995572a7a92bd967fa
Douglas Counts <douglascounts@gmail.com>
2021-09-25 04:46:21 -0500

    🌟 Init the repo
    First deposit to the repo of some good commit message icons.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Git Commit Icons
+
+These are just some handy icons I have come across from time to time to give some graphic impact to my commit messages.
+
+-Doug
---
+++ icons.js
@@ -0,0 +1,22 @@
+//
+//  These are some useful icons to use in Git Commit messages:
+//
+// 💣 bomb
+// 💄 lip stick for better style/theme
+// 🚚 moved to entry app.svelte
+// 🧙‍♂️ working details confirm btn
+// ✅ always more todo / wishlist 
+// 🍕 toppings updated
+// 🌟 favicon updated
+// 🌟 Init the repo
+// 🅱 Changed the branding
+// 🤑 added price
+// 🐱‍👤 scrollTo hide mobile address bar
+// 📱 better for mobile experience
+// 🧹 entry point, and style cleanup
+// 🚀 deploy / Launch
+// 🔀 onDragend / Routes
+// 📦 added packaging
+// 🤔 slider or no slider
+// 🐣 chicken or egg?
+
